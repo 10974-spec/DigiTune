5fa89ecd1d43870fb1ab7dc9a1af733a99e65f94
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-13 22:17:33 +0300

    play with id

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -6,7 +6,7 @@ const SongItem = ({name, image, desc,id}) => {
   const {playwithId} = useContext(PlayerContext)
 
   return (
-    <div className='min-w-[180px]p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+    <div onClick={()=>playwithId(id)} className='min-w-[180px]p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
         <img className='rounded' src={image} alt="" />
         <p className='font-bold mt-2  md-1'>{name}</p>
         <p className='text-slate-200text-sm'>{desc}</p>
