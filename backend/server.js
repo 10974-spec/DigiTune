c6014c9003284a20c282867a3b8cedf3b8d1bc03
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-15 21:04:52 +0300

    album controller

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import songRouter from './src/routes/songRoute.js'
 import connectDB from './src/config/mongodb.js'
 import connectCloudinary from './src/config/cloudinary.js'
+import albumRouter from './src/routes/albumRoute.js'
 
 
 // APP CONFIG  
@@ -21,6 +22,7 @@ app.use(express.json())
 // ROUTES
 
 app.use("/api/song", songRouter) 
+app.use("/api/album", albumRouter)
     
 
 app.get('/', (req, res) => {
